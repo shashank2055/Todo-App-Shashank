fa526339b3d40a1d7094ae504569919258c858a9
shashank gaihre <sharmashashank2055@gmail.com>
2021-12-22 12:39:00 +0545

    list fragment for todolist is added

---
+++ app/src/main/java/com/example/todoapp/TodosListFragment.java
@@ -0,0 +1,30 @@
+package com.example.todoapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TodosListFragment#} factory method to
+ * create an instance of this fragment.
+ */
+public class TodosListFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo_list, container, false);
+    }
+}
